b27a3f943c62dfbafecbe88938dd8f22a04e1a96
dh-r-uv <Dhruv.Kothari@iitb.ac.in>
2024-04-25 17:47:57 +0530

    Set_up done

--- Reed_Solomon.cpp
+++ Reed_Solomon.cpp
@@ -1,70 +1,54 @@
 #include <bits/stdc++.h>
 #include "bigint.h"
 using namespace std;
+ #define int __int128
+ //#define int long long
 #define pii pair<int, int>
 #define vi vector<int>
 #define vii vector<pii>
 #define pb push_back
 #define F first
 #define S second
-
-/*debug start*/
- void __print32_t(int32_t x) {cerr << x;}
-void __print32_t(long x) {cerr << x;}
-void __print32_t(long long x) {cerr << x;}
-void __print32_t(unsigned x) {cerr << x;}
-void __print32_t(unsigned long x) {cerr << x;}
-void __print32_t(unsigned long long x) {cerr << x;}
-void __print32_t(float x) {cerr << x;}
-void __print32_t(double x) {cerr << x;}
-void __print32_t(long double x) {cerr << x;}
-void __print32_t(char x) {cerr << '\'' << x << '\'';}
-void __print32_t(const char *x) {cerr << '\"' << x << '\"';}
-void __print32_t(const string &x) {cerr << '\"' << x << '\"';}
-void __print32_t(bool x) {cerr << (x ? "true" : "false");}
- 
-template<typename T, typename V>
-void __print32_t(const pair<T, V> &x) {cerr << '{'; __print32_t(x.first); cerr << ','; __print32_t(x.second); cerr << '}';}
-template<typename T>
-void __print32_t(const T &x) {int32_t f = 0; cerr << '{'; for (auto &i: x) cerr << (f++ ? "," : ""), __print32_t(i); cerr << "}";}
-void _print32_t() {cerr << "]\n";}
-template <typename T, typename... V>
-void _print32_t(T t, V... v) {__print32_t(t); if (sizeof...(v)) cerr << ", "; _print32_t(v...);}
-#ifndef ONLINE_JUDGE
-#define debug(x...) cerr << "[" << #x << "] = ["; _print32_t(x)
-#else
-#define debug(x...)
-#endif
- /*debug end*/
+//#define int bigint
 
 
-//n > 2MP^2 to be able to recover it
-
-string s;
-bigint M;
-void setM(){
-    for(int i=0; i<50; i++) s.pb(rand()%10);
-    M = bigint(s);
-}
-
-vi primes;
-
-
-
-
-int bin_egcd(int a, int b, int &x, int &y) {
-    if (!a || !b){
-        if(a) x=1, y=0;
-        else x=0, y=1;
-        return a | b;
+/*in/out of u128 setup start*/
+__int128 read() {
+    __int128 x = 0, f = 1;
+    char ch = getchar();
+    while (ch < '0' || ch > '9') {
+        if (ch == '-') f = -1;
+        ch = getchar();
     }
-    unsigned shift = __builtin_ctz(a | b);
+    while (ch >= '0' && ch <= '9') {
+        x = x * 10 + ch - '0';
+        ch = getchar();
+    }
+    return x * f;
+}
+void print(__int128 x) {
+    if (x < 0) {
+        putchar('-');
+        x = -x;
+    }
+    if (x > 9) print(x / 10);
+    putchar(x % 10 + '0');
+}
+/*in/out of u128 setup end*/
+
+
+/*extra functions start*/
+int bin_egcd(int a, int b, int &x, int &y) {
+    if (a==0 || b==0){
+        if(a!=0){x=1, y=0; return a;}
+        else {x=0, y=1; return b;}
+    }
+    int shift = __builtin_ctz(a | b);
     int rx = a, ry = b;
     int sx = 1, sy = 0, tx = 0, ty = 1;
     int cnt=0;
     while(rx!=ry){
         cnt++;
-        debug(cnt, rx, ry, sx, sy, tx, ty);
         if(rx&1){
             if(rx>ry){
                 rx = rx-ry;
@@ -93,26 +77,176 @@ int bin_egcd(int a, int b, int &x, int &y) {
     y = tx;
     return rx << shift;
 }
-
-
-int combine_CRT(vector<pii> &a){
-    int n = a.size();
-    int ans = 0;
-    int M = 1;
-    for(int i=0; i<n; i++){
-        M *= a[i].first;
+//egcd that does rational reconstruction
+void Rational_reconstruction(int n, int b, int R, int T, int &r, int &t, int &s){
+    int r_cur = n, r_prev = b;
+    int s_cur = 1, t_cur = 0, s_old = 0, t_old = 1;
+    while(r_prev>R){
+        int q = r_cur/r_prev;
+        tie(r_cur, r_prev) = make_tuple(r_prev, r_cur-q*r_prev);
+        tie(s_cur, s_old) = make_tuple(s_old, s_cur-q*s_old);
+        tie(t_cur, t_old) = make_tuple(t_old, t_cur-q*t_old);
     }
-    for(int i=0; i<n; i++){
-        int Mi = M/a[i].first;
-        int x, y;
-        bin_egcd(Mi, a[i].first, x, y);
-        ans += a[i].second*Mi*x;
-    }
-    return ans%M;
+    r = r_prev, s = s_old, t = t_old;
 }
 
-int main(){
-    int x, y;
-    cout << bin_egcd(20, 130, x,y) <<endl;
-    cout << x << " "<< y<<endl;
+int reconstruct_CRT(vector<pii> &a){
+    int N = 1;
+    for(auto x:a){
+        N*=x.F;
+    }
+    int soln = 0;
+    for(auto x:a){
+        int Ni = N/x.F;
+        int y, z;
+        bin_egcd(Ni, x.F, y, z);
+        soln = (soln + Ni*y*x.S)%N;
+    }
+    return soln%N;
 }
+
+/*extra functions end*/
+
+/*Miller Rabin*/
+ int fast_pow(int a, int b, int m){
+    int ans = 1;
+    while(b){
+        if(b&1) ans = (ans*a)%m;
+        a = (a*a)%m; b>>=1;
+    }
+    return ans;
+}
+
+bool check_witness(int a, int s, int d, int n){
+    int x = fast_pow(a, d, n);
+    if(x==1 || x==n-1) return false;
+    for(int i=0; i<s-1; i++){
+        x = (x*x)%n;
+        if(x==1) return true;
+        if(x==n-1) return false;
+    }
+    return true;
+}
+
+bool MillerRabin(int n, int iter = 10){
+    if(n<4)
+        return n==2 || n==3;
+    if(n%2==0) return false;
+    int d = n-1;
+    int s = __builtin_ctz(d);
+    d>>=s;
+
+    for(int i=0; i<iter; i++){
+        int a = 2 + rand()%(n-3); //checking if a is a witness
+        if(check_witness(a, s, d, n))
+            return false;
+    }
+    return true;
+}
+
+/*Miller Rabin end*/
+
+
+
+
+
+/*Actual program Starts here*/
+vi primes;
+
+int M = 1000;
+float u = 0.2;
+int k = 10;
+int N = 1;
+int P = 1;
+
+
+//input is A, M, u, k
+void Global_Setup(){
+    int n = 10000000;
+    vi lp(n+1);
+    int c = 0;
+    for (int i=2; i <= n; ++i) {
+        if (lp[i] == 0) {
+            lp[i] = i;
+            primes.push_back(i);
+            c++;
+            if(c>k) break;
+        }
+        for (int j = 0; i * primes[j] <= n; ++j) {
+            lp[i * primes[j]] = primes[j];
+            if (primes[j] == lp[i]) {
+                break;
+            }
+        }
+    }
+    reverse(primes.begin(), primes.end()); // Reverse the elements in the 'primes' vector
+    primes.pop_back();
+    for(auto prime:primes){
+        N*=prime;
+    }
+    for(int i=0; i<floor(u*k); i++){
+        P*=primes[i];
+    }
+    assert(N>2*M*P*P);
+}
+vii Transmit(vii &a){
+    int lmax = floor(u*k), l = rand()%(lmax+1);
+    set<int> corrupted;
+    vii b;
+    for(int i=0; i<l; i++){
+        corrupted.insert(rand()%k);
+    }
+    for(int i=0; i<k; i++){
+        if(corrupted.count(i))
+            b.pb({a[i].F, rand()%a[i].F});
+        else 
+            b.pb({a[i].F, a[i].S});        
+    } 
+    return b;
+}
+
+vii ReedSolomonSend(int a){
+    vii ai;
+    for(int i=0; i<k; i++){
+        ai.pb({primes[i], a%primes[i]});
+    }
+    return Transmit(ai);    
+}
+
+
+int ReedSolomonReceive(vii &b){
+    int B = reconstruct_CRT(b);
+    //cout << "Corrupted Message received: " << B << "\n";
+    cout << "Corrupted Message received: ";
+    print(B);
+    cout << "\n";
+    int r, t, s;
+    cout << "Reconstructing the Message......\n";
+    Rational_reconstruction(N, B, M*P, P, r, t, s);
+    if(r%t) return -1;
+    else return r/t;    
+}
+
+int32_t main(){
+    cout << "Enter the max bound of the message: ";
+    //cin >> M;
+    M = read();
+    cout << "Enter the fraction of corruption: ";
+    cin >> u;
+    // u = read();
+    cout << "Enter the number of primes: ";
+    //cin >> k; 
+    k = read();
+    Global_Setup();
+    cout << "Global Setup Done\n";
+    cout << "Enter the message to be transmitted: ";
+    int message;
+    //cin >> message;
+    message = read();
+    cout << "Transmitting message.........\n";
+    vii a = ReedSolomonSend(message);
+    int message_rec = ReedSolomonReceive(a);
+    cout<<"Message after Reconstruction: ";
+    print(message_rec);
+}
+/*Actual program ends here*/
